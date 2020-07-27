82d02055c80ef48f816eececf59306f8c9def538
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-28 01:17:57 +0530

    Using Cloud Sleuth

--- src/main/java/com/boot/demo/PriceController.java
+++ src/main/java/com/boot/demo/PriceController.java
@@ -1,5 +1,7 @@
 package com.boot.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PriceController {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(PriceController.class);
+	
 	@RequestMapping("/getPrice")
 	public @ResponseBody PriceData getProductPrice(@RequestBody String productCode)
 	{
-		System.out.println("Fetching Price for "+productCode);
+		LOG.info("inside getProductPrice...");
+		
+		LOG.info("Fetching Price for "+productCode);
 		
 		PriceData data = new PriceData();
 		data.setUnitPrice(1800.00);
